c930abf4bf9d825fcc3cb5dc65e4e081591af7df
jacobward1898 <jacobward1898@gmail.com>
2014-11-18 15:23:26 -0700

    Created Xcode Project

--- RotatingObject/Jacob/Jacob/main.cpp
+++ RotatingObject/Jacob/Jacob/main.cpp
@@ -1,15 +1,23 @@
+//
+//  main.cpp
+//  Jacob
+//
+//  Created by Jacob Ward on 11/18/14.
+//  Copyright (c) 2014 Jacob Ward. All rights reserved.
+//
+
 #include <iostream>
 #include <stdarg.h>
 #include <math.h>
 #define GL_GLEXT_PROTOTYPES
 
 #ifdef __APPLE__	//If it is running on an Apple device
-	#include <GLUT/glut.h>
+#include <GLUT/glut.h>
 #else				//If it is running on anything else
-	#include <GL/glut.h>
+#include <GL/glut.h>
 #endif
 
 int main(int argc, char* argv[]){
-
-
+    
+    
 }
