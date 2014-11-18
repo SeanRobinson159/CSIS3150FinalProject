f60889e0dd7d4916c180f3015ac66bd97b26706d
jacobward1898 <jacobward1898@gmail.com>
2014-11-18 16:12:17 -0700

    Added GLFW - JCW

--- RotatingObject/Jacob/Jacob/main.cpp
+++ RotatingObject/Jacob/Jacob/main.cpp
@@ -9,15 +9,10 @@
 #include <iostream>
 #include <stdarg.h>
 #include <math.h>
+#include "glfw3.h"
+
 #define GL_GLEXT_PROTOTYPES
 
-#ifdef __APPLE__	//If it is running on an Apple device
-#include <GLUT/glut.h>
-#else				//If it is running on anything else
-#include <GL/glut.h>
-#endif
-
 int main(int argc, char* argv[]){
-    
     
 }
