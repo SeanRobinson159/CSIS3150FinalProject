2c6f959e5089d23efb799693c4c4a6a586475c67
SeanRobinson159 <SeanRobinson159@gmail.com>
2014-11-18 16:21:38 -0700

    Cube!

---
+++ RotatingObject/Sean/Cube/Cube/main.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include "glfw3.h"
+#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__    //If it is an Apple device
+    #include <GLUT/glut.h>
+#else
+    #include <GL/glut.h>
+#endif
+
+// ----------------------------------------------------------
+// Function Prototypes
+// ----------------------------------------------------------
+void display();
+void specialKeys();
+
+double rotate_y=0;
+double rotate_x=0;
+bool autoRotate=false;
+
+void display(){
+    
+    //  Clear screen and Z-buffer
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    
+    // Reset transformations
+    glLoadIdentity();
+    
+    // Rotate when user changes rotate_x and rotate_y
+    glRotatef( rotate_x, 1.0, 0.0, 0.0 );
+    glRotatef( rotate_y, 0.0, 1.0, 0.0 );
+    
+    //Dark - FRONT
+    glBegin(GL_POLYGON);
+    glColor3f(   0.3,  0.3,  0.3 );
+    glVertex3f(  0.5, -0.5, -0.5 );
+    glVertex3f(  0.5,  0.5, -0.5 );
+    glVertex3f( -0.5,  0.5, -0.5 );
+    glVertex3f( -0.5, -0.5, -0.5 );
+    glEnd();
+    
+    // White side - BACK
+    glBegin(GL_POLYGON);
+    glColor3f(   1.0,  1.0, 1.0 );
+    glVertex3f(  0.5, -0.5, 0.5 );
+    glVertex3f(  0.5,  0.5, 0.5 );
+    glVertex3f( -0.5,  0.5, 0.5 );
+    glVertex3f( -0.5, -0.5, 0.5 );
+    glEnd();
+    
+    // Purple side - RIGHT
+    glBegin(GL_POLYGON);
+    glColor3f(  1.0,  0.0,  1.0 );
+    glVertex3f( 0.5, -0.5, -0.5 );
+    glVertex3f( 0.5,  0.5, -0.5 );
+    glVertex3f( 0.5,  0.5,  0.5 );
+    glVertex3f( 0.5, -0.5,  0.5 );
+    glEnd();
+    
+    // Green side - LEFT
+    glBegin(GL_POLYGON);
+    glColor3f(   0.0,  1.0,  0.0 );
+    glVertex3f( -0.5, -0.5,  0.5 );
+    glVertex3f( -0.5,  0.5,  0.5 );
+    glVertex3f( -0.5,  0.5, -0.5 );
+    glVertex3f( -0.5, -0.5, -0.5 );
+    glEnd();
+    
+    // Blue side - TOP
+    glBegin(GL_POLYGON);
+    glColor3f(   0.0,  0.0,  1.0 );
+    glVertex3f(  0.5,  0.5,  0.5 );
+    glVertex3f(  0.5,  0.5, -0.5 );
+    glVertex3f( -0.5,  0.5, -0.5 );
+    glVertex3f( -0.5,  0.5,  0.5 );
+    glEnd();
+    
+    // Red side - BOTTOM
+    glBegin(GL_POLYGON);
+    glColor3f(   1.0,  0.0,  0.0 );
+    glVertex3f(  0.5, -0.5, -0.5 );
+    glVertex3f(  0.5, -0.5,  0.5 );
+    glVertex3f( -0.5, -0.5,  0.5 );
+    glVertex3f( -0.5, -0.5, -0.5 );
+    glEnd();
+    
+    if (autoRotate) {
+        rotate_x +=1;
+        rotate_y +=1;
+        glutPostRedisplay();
+    }
+    glFlush();
+    glutSwapBuffers();
+    
+}
+
+// ----------------------------------------------------------
+// specialKeys() Callback Function
+// ----------------------------------------------------------
+void specialKeys( int key, int x, int y ) {
+    if (key == GLUT_KEY_RIGHT)  //  Right arrow - increase rotation by 5 degree
+        rotate_y += 5;
+    else if (key == GLUT_KEY_LEFT) //  Left arrow - decrease rotation by 5 degree
+        rotate_y -= 5;
+    else if (key == GLUT_KEY_UP)
+        rotate_x += 5;
+    else if (key == GLUT_KEY_DOWN)
+        rotate_x -= 5;
+    else if (key == 27)     //Esc key
+        exit(0);
+    else if (key == 32){    //SpaceBar
+        if (autoRotate) {
+            autoRotate = false;
+        } else {
+            autoRotate = true;
+        }
+    }
+    //  Request display update
+    glutPostRedisplay();
+}
+
+// ----------------------------------------------------------
+// main() function
+// ----------------------------------------------------------
+int main(int argc, char* argv[]){
+    //  Initialize GLUT and process user parameters
+    glutInit(&argc,argv);
+    //  Request double buffered true color window with Z-buffer
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    // Create window
+    glutCreateWindow("Awesome Cube");
+    //  Enable Z-buffer depth test
+    glEnable(GL_DEPTH_TEST);
+    // Callback functions
+    glutDisplayFunc(display);
+    glutSpecialFunc(specialKeys);
+    //  Pass control to GLUT for events
+    glutMainLoop();
+    
+    //  Return to OS
+    return 0;
+    
+}
