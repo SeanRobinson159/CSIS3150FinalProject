7dbee82a4799cc460e3da77b5611ea8124b91531
Mindy <dewaal.mindy@gmail.com>
2014-11-20 09:14:48 -0700

    Teapot
    Call teapot and it should work :P

--- RotatingObject/Mindy/Main.cpp
+++ RotatingObject/Mindy/Main.cpp
@@ -1,101 +1,29 @@
-#include <stdlib.h>
-//#include <GL/glu.h>
+#ifndef SHAPEHEADER_H_INCLUDED
+#define SHAPEHEADER_H_INCLUDED
+#endif // SHAPEHEADER_H_INCLUDED
 
-#ifdef __APPLE__	//If it is running on an Apple device
-	#include <GLUT/glut.h>
-#else				//If it is running on anything else
-	#include <GL/glut.h>
+#include <iostream>
+#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__    //If it is an Apple device
+    #include <GLUT/glut.h>
+#else
+    #include <GL/glut.h>
 #endif
 
-
-static int slices = 20;
-static int stacks = 20;
-
-static void resize(int width, int height)
-{
-    const float area = (float) width / (float) height;
-
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glFrustum(-area, area, -1.0, 1.0, 2.0, 50.0);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity() ;
-}
-
-static void display(void)
-{
-    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*50.0;
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(0,1,1);
-
-    glPushMatrix();
-        glTranslated(0,0,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidSphere(2,slices,stacks);
-        //glutSolidTeapot(1.75);
-    glPopMatrix();
-
-    glutSwapBuffers();
-}
+#include <GL/glu.h>
+#include <stdlib.h>
 
 
-static void idle(void)
-{
-    glutPostRedisplay();
-}
 
-const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
+void teapot() {
+glColor3f(1,0.0,0.0);
+glPushMatrix();
+glutWireTeapot(.5);
+glPopMatrix();
 
-const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 10.0f };
-const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat shininess[] = { 20.0f };
-
-
-int main(int argc, char *argv[])
-{
-    glutInit(&argc, argv);
-    glutInitWindowSize(400,400);
-    glutInitWindowPosition(10,10);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-
-    glutCreateWindow("Sphere >.<");
-
-    glutReshapeFunc(resize);
-    glutDisplayFunc(display);
-    glutIdleFunc(idle);
-
-    glClearColor(1,20,1,20);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-
-    glEnable(GL_LIGHT0);
-    glEnable(GL_NORMALIZE);
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_LIGHTING);
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, shininess);
-
-    glutMainLoop();
-
-    return EXIT_SUCCESS;
+    if (autoRotate) {
+        rotate_x +=1;
+        rotate_y +=1;
+        glutPostRedisplay();
+    }
 }
